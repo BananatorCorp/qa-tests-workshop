2fef9943f6856f3d46598e6de073878c12472d82
BananatorCorp <56715651+BananatorCorp@users.noreply.github.com>
2019-11-05 11:16:06 +0100

    Test 1.1 done
    Please refer to the README.md to have more information about those tests

--- cypress/integration/open-new-tab.spec.js
+++ cypress/integration/open-new-tab.spec.js
@@ -1,8 +1,15 @@
 /// <reference types="Cypress" />
 
 describe('Source open in a new tab', () => {
-    it('can cy.visit', () => {
-        cy.visit("https://mds-qa-tests.angry-unicorn.space/")
+    it('Click on a news title open it in a new tab', () => {
+        cy.visit("https://mds-qa-tests.angry-unicorn.space/") //place ourselves at the right site in order to pass tests
+
+
+        // click on the first news title of the list to see if it opens on a new tab and scan to see if it has an
+        // attribut who opened the link on a new tab
+        cy.get(':nth-child(1) > .title > a')
+            .click()
+        cy.get(':nth-child(1) > .title > a').should('have.attr', 'target', '_blank')
     })
 
 })
